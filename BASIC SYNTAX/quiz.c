997edc634d2fbc467a1959a3b63fbe82ec62c3de
MD AMAN <mrc3824@gmail.com>
2024-09-10 12:20:39 +0530

    simple quiz

---
+++ BASIC SYNTAX/quiz.c
@@ -0,0 +1,23 @@
+/*write a c program to calculate are of a rectangle
+using input supiler by user
+area of rectangle is lenght*breath
+lets goo...*/
+//practice set 1..
+
+# include<stdio.h>
+
+int main() {
+    // int a= 8;
+    // int b= 5;
+    int lenght,breath;
+
+    printf("enter lenght\n");
+    scanf("%d",&lenght);  //sapose that lenght is 6
+
+       printf("enter breath\n");
+    scanf("%d",&breath);  //sapose that breath is 5
+
+    printf("the are of this rectangle is %d",lenght*breath); //then you do all things right so answer is 30
+
+    return 0;
+}
