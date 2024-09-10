c5bc3fecdc5470fecf984f02afae13861286aebd
MD AMAN <mrc3824@gmail.com>
2024-09-10 11:58:55 +0530

    new file

---
+++ BASIC SYNTAX/scanf.c
@@ -0,0 +1,19 @@
+//scan function is used to collect input from user
+
+# include<stdio.h>
+int main() {
+    int a;
+       scanf("%d", &a);
+       printf("the value of a is %d",a);
+    return 0;
+}
+
+// multi int..
+# include<stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d + %d = %d",a,b,a+b);
+    return 0;
+}
