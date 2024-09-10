c2c7883d9790f19ab1220e01d3ca9c31cbce4782
MD AMAN <mrc3824@gmail.com>
2024-09-10 12:17:51 +0530

    add code

--- BASIC SYNTAX/scanf.c
+++ BASIC SYNTAX/scanf.c
@@ -17,3 +17,40 @@ int main() {
     printf("%d + %d = %d",a,b,a+b);
     return 0;
 }
+
+// sum of a + b = c..
+
+# include<stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 3;
+    int c = a + b;
+    printf("the value of a is %d and the value of b is %d and the sum is %d",a,b,c);
+    return 0;
+}
+
+// sum of a + b = c with scanf..
+
+# include<stdio.h>
+
+int main() {
+    int a = 5;
+    int b;
+    int c = a+b;
+    scanf("%d",&b);
+    printf("the value of a is %d and the value of b is %d and the sum is %d",a,b,c);
+    return 0;
+}
+
+// finding rimender.. 
+
+# include<stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 3;
+    int c = a % b;
+    printf("the rimender of a is divided by b is %d",a%b);
+    return 0;
+}
